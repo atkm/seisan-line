08147fd7d4a22f928471fe5412531bb17c08589f
Atsuya <atsuya@puppetlabs.com>
2012-07-26 19:43:48 -0700

    origami - seisan integration

--- bin/seisan.rb
+++ bin/seisan.rb
@@ -18,3 +18,56 @@
 ### build <name>: build a <name> fusion box
 ### build <name> --headless: without GUI
 ### --help: show help
+
+if ARGV.empty?
+  puts 'do: --help'
+end
+
+project_root = File.expand_path(
+                                File.join(File.dirname(__FILE__), '..')
+                                )
+
+project_path = File.join( project_root, 'lib')
+origami_path = File.join( project_root, 'origami/bin')
+$veewee_path = File.join( project_root, 'veewee')
+definitions_path = File.join( project_root, 'definitions')
+
+$LOAD_PATH.unshift(project_path) unless $LOAD_PATH.include?(project_path)
+$LOAD_PATH.unshift(origami_path) unless $LOAD_PATH.include?(origami_path)
+
+require 'optparse'
+require 'optparsing'
+require 'origami'
+
+def build(name,options)
+  command = "cd #{$veewee_path} && bundle exec veewee fusion build "
+  if options[:headless]
+    command = command + '-n '
+  end
+  if options[:force]
+    command = command + '-f '
+  end
+  system(command + name)
+end
+
+options = SeisanLine::Options.parse_args
+
+## calling 'veewee build'
+## I'm not going to figure out how 'Thor' parses options
+## hence, using system call.
+if options[:build]
+  name = options[:name]
+  puts "Initiating the build of #{name}..."
+  puts "Options are: "
+  puts "\t headless => #{options[:headless]}; force => #{options[:force]}."
+  build(name,options)
+end
+
+
+## call 'origami <name>' 
+if options[:define]
+  build_from_seed(options[:name],'kickstart',definitions_path)
+  puts
+  build_from_seed(options[:name],'definition',definitions_path)
+  puts
+end
