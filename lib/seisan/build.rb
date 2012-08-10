3d1ce41a593b96c03016dae9e2d81229e26e57e1
Atsuya <atsuya@puppetlabs.com>
2012-08-10 12:23:45 -0700

    right before bootstrap-test

--- lib/seisan/build.rb
+++ lib/seisan/build.rb
@@ -9,16 +9,6 @@ module Seisan
     print("Passing the work to veewee...\n\n")
 
     command = veewee_command + 'build '
-    if options[:bootstrap]
-      puts "Bootstrap option was selected."
-      $LOAD_PATH.unshift(origami_path) unless $LOAD_PATH.include?(origami_path)
-      require 'origami'
-      require 'seisan/define'
-      name = options[:name]
-      define(name)
-      puts "Definition for #{name} has been created."
-      puts "Now initiating its build."
-    end
     if options[:headless]
       command = command + '-n '
     end
